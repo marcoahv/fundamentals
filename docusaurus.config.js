e6cb49cda0d3cd627f61f568f4e8a80236e43b7d
Marco Hernandez <MarcoAHVega@gmail.com>
2023-06-19 22:49:11 -0700

    initial commit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: 'https://github.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: 'MarcoAHVega/fundamentals/',
+  baseUrl: '/fundamentals/',
   deploymentBranch: 'gh-pages',
   trailingSlash: false,
 
